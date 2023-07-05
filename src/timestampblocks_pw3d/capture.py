da7990680f1b87d2e0f218aace59d24ed4bc0918
Christof Spanring <spanring@@users.noreply.github.com>
2023-07-05 16:54:36 +0200

    timestampblocks update for root e0f84c7ff7be0a5d0d3d163b0945c5627633e27be0e9db44283d03744212903154aecedf11735caff20e7cdeab5ed88c

--- src/timestampblocks_pw3d/capture.py
+++ src/timestampblocks_pw3d/capture.py
@@ -8,6 +8,11 @@
 #.timestampblocks.ignore
 # - .timestampblocks.*
 
+#TODO:
+# contract and logic for evm publishing (add publishing arguments to constructor in case we need to deploy first)
+# rethink encoding of message, possibly different for evm
+# -> ha, for now we simply send transactions with block_root as to_address
+
 import configparser
 import argparse
 import hashblock
@@ -17,11 +22,12 @@ import pathspec
 from pathlib import Path
 import time
 from datetime import datetime
-from dotenv import load_dotenv
+from dotenv import dotenv_values
+from web3 import Web3
 
 from iota_client import IotaClient
 
-_possible_publish = ("git", "iota", "polygon", "shell", "ethereum")
+_possible_publish = ("git", "iota", "shell", "evm")
 _available_commands = ("update", "query-settings")
 _tsb_dir = ".timestampblocks/"
 if not os.path.exists(_tsb_dir):
@@ -34,7 +40,7 @@ _log_version=1
 _blocksize = 65536
 _publish_even_if_no_changes = False
 
-load_dotenv()
+_dotenv = dotenv_values(".env")
 
 _config = {
     "default": {
@@ -42,8 +48,15 @@ _config = {
         "hashing": "sha384",
     },
     "iota": {
+        "protocol": "iota",
         "node": "https://iota-node.tanglebay.com",
     },
+    "evm": {
+        "protocol": "evm",
+        "node": "https://goerli.infura.io/v3/",
+        "api-key": "INFURA_SECRET",
+        "private-key": "EVM_SECRET",
+    }
 }
 
 def main():
@@ -66,6 +79,8 @@ def main():
                         metavar="channel",
                         help="options are "+str(_possible_publish)+",\n"+
                         "(setting is " + str(settings["default"]["publish"].split())+")")
+    parser.add_argument('-d', '--dummy', action="store_true",
+                        help="only print on shell and do not push or write block updates otherwise")
 #    parser.add_argument('-m', '--message', nargs=1, metavar="text",
 #                        help="optional message where publishing channel allows a message")
 #    parser.add_argument('-', '--parameters', nargs="+",
@@ -89,6 +104,7 @@ def main():
     command = args.command[0]
     if command == "query-settings":
 #        assert len(params) == 0, "no parameters allowed for this command!"
+        assert not args.dummy, "settings update not possible as dummy version yet"
         settings = query_configuration(settings)
         with open(_config_file, "w") as configfile:
             settings.write(configfile)
@@ -97,27 +113,41 @@ def main():
         new_hashes = get_new_hashes(hash_set, settings["default"]["hashing"])
         if len(new_hashes) > 0 or _publish_even_if_no_changes:
             new_block = build_block(new_hashes, last_root, last_proper_root, settings["default"]["hashing"])
-            with open(_log_file, "a") as f:
-                if (last_root != last_proper_root):
-                    f.write("#hashing "+settings["default"]["hashing"]+"\n")
-                f.write(new_block["data"]+"\n")
-                f.write("#root " + new_block["root"]+"\n")
+            if not args.dummy:
+                with open(_log_file, "a") as f:
+                    if (last_root != last_proper_root):
+                        f.write("#hashing "+settings["default"]["hashing"]+"\n")
+                    f.write(new_block["data"]+"\n")
+                    f.write("#root " + new_block["root"]+"\n")
+            else:
+                print("-dummy-", "not actually writing log file")
             publishers = settings["default"]["publish"].split()
             for channel in publishers:
-                publish(channel, new_block, args.assume_yes)
+                publish(channel, new_block, args.assume_yes, args.dummy)
         else:
             print("no updates detected")
 
-def publish(channel, block, assume_yes):
+def publish(channel, block, assume_yes, dummy):
     response = None
     if channel == "shell":
-        response = publish_shell(block, assume_yes)
+        response = publish_shell(block, assume_yes, dummy)
     elif channel == "git":
-        response = publish_git(block, assume_yes)
+        response = publish_git(block, assume_yes, dummy)
+    elif channel == "iota":
+        response = publish_iota(block, assume_yes, dummy)
     else:
-        print("Not implemented yet!", channel, block, assume_yes)
+        settings = configparser.ConfigParser()
+        settings.read(_config_file)
+        if settings.has_section(channel):
+            if settings[channel]["protocol"] == "evm":
+                response = publish_evm(block, assume_yes, dummy, channel=channel)
+            else:
+                print("Custom channel not implemented yet!", channel, block, assume_yes, dummy)
+        else:
+            print("Channel not implemented yet!", channel, block, assume_yes, dummy)
     if response != None:
         # TODO log the response (likely a transaction id)
+        print("response:", response)
         pass
 
 #    elif publishing_method == 'iota':
@@ -131,7 +161,31 @@ def publish(channel, block, assume_yes):
 #    elif publishing_method == 'ethereum':
 #        pass
 
-def publish_iota(block, assume_yes):
+def publish_evm(block, assume_yes, dummy=False, channel="evm"):
+    settings = configparser.ConfigParser()
+    settings.read(_config_file)
+    if not settings.has_section(channel):
+        settings = _config
+    web3 = Web3(Web3.HTTPProvider(settings[channel]["node"] + _dotenv[settings[channel]["api-key"]]))
+    account = web3.eth.account.from_key(_dotenv[settings[channel]["private-key"]])
+    nonce = web3.eth.getTransactionCount(account.address)
+    recipient = web3.toChecksumAddress("0x" + block["root"][:40]) #trick to publish block_root via to_address
+    tx = {
+        "nonce": nonce,
+        "to": recipient,
+        "value": 0,
+        "gas": 20000000,
+        "gasPrice": web3.eth.gasPrice
+    }
+    signed_tx = account.sign_transaction(tx)
+#    signed_tx = web3.eth.account.sign.transaction(tx, _dotenv[settings[channel]["private-key"]])
+    if dummy:
+        print("-dummy-", "not publishing on", channel, signed_tx)
+    else:
+        tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        return tx_hash
+
+def publish_iota(block, assume_yes, dummy=False):
     settings = configparser.ConfigParser()
     settings.read(_config_file)
     node_uri = _config["iota"]["node"]
@@ -142,10 +196,13 @@ def publish_iota(block, assume_yes):
         "tag": "0x" + "timehashblock".encode("utf-8").hex(),
         "data": block["root"],
     }
-    response = client.build_and_post_block(None, options)
-    return response[0]
+    if dummy:
+        print("-dummy-", "not publishing on iota", options)
+    else:
+        response = client.build_and_post_block(None, options)
+        return response
 
-def publish_git(block, assume_yes):
+def publish_git(block, assume_yes, dummy=False):
     if len(str(os.popen("git check-ignore .env").read())) == 0:
         print(".env file is not excluded from git!")
         print("Add .env to .gitignore? (Yn)")
@@ -166,7 +223,10 @@ def publish_git(block, assume_yes):
         else:
             inp = input()
         if inp != "n":
-            os.system("git add --force " + _log_file)
+            if dummy:
+                print("-dummy-", "not running `git add --force", _log_fle + "`")
+            else:
+                os.system("git add --force " + _log_file)
     print("Git submission:")
     if not assume_yes:
         os.system("git add -n .")
@@ -174,17 +234,24 @@ def publish_git(block, assume_yes):
         if input() == "n":
             print("Aborting...")
             return
-    os.system("git add .")
+    if dummy:
+        print("-dummy-", "not running `git add .`")
+    else:
+        os.system("git add .")
     if not assume_yes:
         os.system("git status")
         print("Proceed? (Yn)")
         if input() == "n":
             print("Aborting...")
             return
-    os.system("git commit -a -m 'timestampblocks update for root " + block["root"] + "'")
-    os.system("git push")
+    if dummy:
+        print("-dummy-", "not running `git commit -a -m 'timestampblocks update for root " + block["root"] + "'`")
+        print("-dummy-", "not running `git push`")
+    else:
+        os.system("git commit -a -m 'timestampblocks update for root " + block["root"] + "'")
+        os.system("git push")
 
-def publish_shell(block, assume_yes):
+def publish_shell(block, assume_yes, dummy=False):
     print("New block with root '"+ block["root"]+"', and data:")
     print(block["data"])
     print("Used hashing algorithm:", block["hashing"])
@@ -284,19 +351,56 @@ def query_configuration(settings):
     if "iota" in channels:
         if not settings.has_section("iota"):
             settings["iota"] = _config["iota"]
-        print("-------")
-        print("IOTA node (" + settings["iota"]["node"] + ")")
-        inp = input()
-        if len(inp) > 0:
-            settings["iota"]["node"] = inp
+        print("------- iota")
+        settings["iota"] = query_protocol("iota", settings["iota"])
+    if "evm" in channels:
+        if not settings.has_section("evm"):
+            settings["evm"] = _config["evm"]
+        print("------- evm")
+        settings["evm"] = query_protocol("evm", settings["evm"])
+    for channel in channels:
+        if not channel in _possible_publish:
+            if not settings.has_section(channel):
+                protocol = "evm"
+            else:
+                protocol = settings[channel].get("protocol", "evm")
+            print("-------", channel)
+            print("protocol (" + protocol +")")
+            inp = input()
+            new_protocol = protocol
+            if len(inp) > 0:
+                new_protocol = inp
+            if not settings.has_section(channel) or protocol != new_protocol:
+                settings[channel] = _config[new_protocol]
+            settings[channel] = query_protocol(channel, settings[channel])
     print("-----------------------")
     print("Default hash algorithm:")
     print(" - available: " + str(hashlib.algorithms_available))
-    print(" - currently: '" + settings["default"]["hasing"]+"'")
+    print(" - currently: '" + settings["default"]["hashing"]+"'")
     inp = input()
     if len(inp) > 0:
         settings["default"]["hashing"] = inp
     return settings
+
+def query_protocol(channel, default_dic, skip_list = ["protocol"]):
+    dd = dict(default_dic).copy()
+    for key in dd:
+        if not key in skip_list:
+            print(key, "(" + dd[key] + ")")
+            inp = input()
+            if len(inp) > 0:
+                dd[key] = inp
+            keysplit = key.split('-')
+            if len(keysplit) > 1 and keysplit[-1] == "key":
+                keyname = key.upper()
+                print(" - store secret as (" + keyname + ") to \".env\" file (no input here means manual editing required)")
+                inp = input()
+                if len(inp) > 0:
+                    with open(".env", "a") as envfile:
+                        envfile.write(dd[key]+"=\""+inp+"\"\n")
+                    global _dotenv
+                    _dotenv = dotenv_values(".env")
+    return dd
 
 if __name__ == "__main__":
     main()
